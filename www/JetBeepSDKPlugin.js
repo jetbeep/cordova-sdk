57607e26b07786d62dd24d56b8eb7e36b1815510
Max Tymchii <samback@ukr.net>
2023-01-18 19:46:16 +0000

    Put back enableBluetooth method. Bump to v.0.0.22

--- www/JetBeepSDKPlugin.js
+++ www/JetBeepSDKPlugin.js
@@ -1,69 +1,73 @@
 var exec = require('cordova/exec');
 
 exports.initSDK = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'initSDK', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'initSDK', [arg0]);
 };
 
 exports.initWithOfflineConfig = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'initWithOfflineConfig', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'initWithOfflineConfig', [arg0]);
 };
 
 exports.searchDevices = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'searchDevices', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'searchDevices', [arg0]);
 };
 
 exports.stopSearching = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'stopSearching', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'stopSearching', [arg0]);
 };
 
 exports.applyToken = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'applyToken', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'applyToken', [arg0]);
 };
 
 exports.isPermissionGranted = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'isPermissionGranted', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'isPermissionGranted', [arg0]);
 };
 
 exports.requestPermissions = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'requestPermissions', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'requestPermissions', [arg0]);
 };
 
 exports.enableBeeper = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'enableBeeper', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'enableBeeper', [arg0]);
 };
 
 exports.subscribeToLocations = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'subscribeToLocations', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'subscribeToLocations', [arg0]);
 };
 
 exports.unsubscribeFromLocations = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'unsubscribeFromLocations', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'unsubscribeFromLocations', [arg0]);
 };
 
 exports.getEnteredShops = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'getEnteredShops', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'getEnteredShops', [arg0]);
 };
 
 exports.getNearbyDevices = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'getNearbyDevices', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'getNearbyDevices', [arg0]);
 };
 
 exports.bluetoothState = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'bluetoothState', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'bluetoothState', [arg0]);
 };
 
 exports.subscribeBluetoothEvents = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'subscribeBluetoothEvents', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'subscribeBluetoothEvents', [arg0]);
 };
 
 exports.unsubscribeBluetoothEvents = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'unsubscribeBluetoothEvents', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'unsubscribeBluetoothEvents', [arg0]);
+};
+
+exports.enableBluetooth = function(arg0, success, error) {
+    exec(success, error, 'JetBeepSDKPlugin', 'enableBluetooth', [arg0]);
 };
 
 exports.subscribeLogEvents = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'subscribeLogEvents', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'subscribeLogEvents', [arg0]);
 };
 
 exports.unsubscribeLogEvents = function(arg0, success, error) {
-  exec(success, error, 'JetBeepSDKPlugin', 'unsubscribeLogEvents', [arg0]);
+    exec(success, error, 'JetBeepSDKPlugin', 'unsubscribeLogEvents', [arg0]);
 };
